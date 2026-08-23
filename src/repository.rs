3dacfaf0ddcb62e6d07745b966bef2978a03d22d
max69-cyber <rayxaus@gmail.com>
2026-08-23 10:27:40 +0500

    выносит общую логику репозиториев в общий файл

--- src/repository.rs
+++ src/repository.rs
@@ -1,5 +1,53 @@
+use std::collections::HashMap;
+
 pub mod products_repository;
 
 pub enum RepositoryError {
     AddingDuplicateItemError,
+}
+
+// trait - это шаблон для impl, можно сказать что это типа интерфейс
+pub trait Identifiable {
+    // чтобы каждый репозиторий смог определить как в нем правильно достать id
+    fn get_id(&self) -> String;
+}
+
+pub struct Repository<T: Identifiable> {
+    items: HashMap<String, T>,
+}
+
+impl<T: Identifiable> Repository<T> {
+    pub fn new() -> Self {
+        Self {
+            items: HashMap::new(),
+        }
+    }
+
+    /// добавляет в хранилище айтем. кидает ошибку в случае если айтем с ключом уже существует
+    pub fn add(&mut self, item: T) -> Result<(), RepositoryError> {
+        let key = item.get_id();
+
+        if self.items.contains_key(&key) {
+            return Err(RepositoryError::AddingDuplicateItemError);
+        };
+
+        self.items.insert(key, item);
+        Ok(())
+    }
+
+    /// проверяет, есть ли в репо айтем по id
+    pub fn exists(&self, id: &str) -> bool {
+        self.items.contains_key(id)
+    }
+
+    /// находит айтем по id и выдает его ссылкой. если его нет - вернет None
+    pub fn find(&self, id: &str) -> Option<&T> {
+        self.items.get(id)
+    }
+
+    /// возвращает вектор ссылок на продукт
+    pub fn get_list(&self) -> Vec<&T> {
+        // values - итератор по содержанию мапы, collect преобразует его в вектор
+        self.items.values().collect()
+    }
 }
--- src/repository/products_repository.rs
+++ src/repository/products_repository.rs
@@ -1,45 +1,11 @@
-use crate::{domain::product::Product, repository::RepositoryError};
-use std::collections::HashMap;
+use crate::domain::product::Product;
+use crate::repository::{Identifiable, Repository};
 
-// хранилище списка продуктов
-pub struct ProductsRepository {
-    // для более быстрого поиска по товарам используется мапа по артикулам
-    products: HashMap<String, Product>,
-}
+pub type ProductsRepository = Repository<Product>;
 
-impl ProductsRepository {
-    pub fn new() -> Self {
-        Self {
-            products: HashMap::new(),
-        }
-    }
-
-    /// добавляет в хранилище товар. кидает ошибку в случае если товар с артикулом уже существует
-    pub fn add(&mut self, product: Product) -> Result<(), RepositoryError> {
-        let key = product.get_article();
-        if self.products.contains_key(key) {
-            return Err(RepositoryError::AddingDuplicateItemError);
-        };
-
-        self.products
-            .insert(product.get_article().to_string(), product);
-
-        Ok(())
-    }
-
-    /// проверяет, есть ли в репо товар по артикулу
-    pub fn exists(&self, article: &str) -> bool {
-        self.products.contains_key(article)
-    }
-
-    /// находит товар по артикулу и выдает его ссылкой. если его нет - вернет None
-    pub fn find(&self, article: &str) -> Option<&Product> {
-        self.products.get(article)
-    }
-
-    /// Возвращает вектор ссылок на продукт
-    pub fn get_list(&self) -> Vec<&Product> {
-        // values - итератор по содержанию мапы, collect преобразует его в вектор
-        self.products.values().collect()
+// видимость методов задается из trait, а тут просто реализуем
+impl Identifiable for Product {
+    fn get_id(&self) -> String {
+        self.get_article().to_string()
     }
 }
